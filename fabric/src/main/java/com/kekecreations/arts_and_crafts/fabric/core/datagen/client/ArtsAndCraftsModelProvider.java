639f665f559c097720808756d08e7962a7dbf5a8
KekeCreations <89083260+KekeCreations@users.noreply.github.com>
2023-11-12 01:12:35 +0000

    All 64 chalk blocks & Block families

--- fabric/src/main/java/com/kekecreations/arts_and_crafts/fabric/core/datagen/client/ArtsAndCraftsModelProvider.java
+++ fabric/src/main/java/com/kekecreations/arts_and_crafts/fabric/core/datagen/client/ArtsAndCraftsModelProvider.java
@@ -1,10 +1,12 @@
 package com.kekecreations.arts_and_crafts.fabric.core.datagen.client;
 
 import com.kekecreations.arts_and_crafts.ArtsAndCrafts;
+import com.kekecreations.arts_and_crafts.core.registry.KekeBlockFamilies;
 import com.kekecreations.arts_and_crafts.core.registry.KekeBlocks;
 import com.kekecreations.arts_and_crafts.core.registry.KekeItems;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricModelProvider;
+import net.minecraft.data.BlockFamily;
 import net.minecraft.data.models.BlockModelGenerators;
 import net.minecraft.data.models.ItemModelGenerators;
 import net.minecraft.data.models.model.ModelTemplates;
@@ -18,22 +20,9 @@ public class ArtsAndCraftsModelProvider extends FabricModelProvider {
 
     @Override
     public void generateBlockStateModels(BlockModelGenerators blockStateModelGenerator) {
-        blockStateModelGenerator.createTrivialBlock(KekeBlocks.BLUE_CHALK.get(), TextureMapping.cube(new ResourceLocation(ArtsAndCrafts.MOD_ID, "block/chalk/blue_chalk")), ModelTemplates.CUBE_ALL);
-        blockStateModelGenerator.createTrivialBlock(KekeBlocks.BLACK_CHALK.get(), TextureMapping.cube(new ResourceLocation(ArtsAndCrafts.MOD_ID, "block/chalk/black_chalk")), ModelTemplates.CUBE_ALL);
-        blockStateModelGenerator.createTrivialBlock(KekeBlocks.BROWN_CHALK.get(), TextureMapping.cube(new ResourceLocation(ArtsAndCrafts.MOD_ID, "block/chalk/brown_chalk")), ModelTemplates.CUBE_ALL);
-        blockStateModelGenerator.createTrivialBlock(KekeBlocks.WHITE_CHALK.get(), TextureMapping.cube(new ResourceLocation(ArtsAndCrafts.MOD_ID, "block/chalk/white_chalk")), ModelTemplates.CUBE_ALL);
-        blockStateModelGenerator.createTrivialBlock(KekeBlocks.LIGHT_BLUE_CHALK.get(), TextureMapping.cube(new ResourceLocation(ArtsAndCrafts.MOD_ID, "block/chalk/light_blue_chalk")), ModelTemplates.CUBE_ALL);
-        blockStateModelGenerator.createTrivialBlock(KekeBlocks.CYAN_CHALK.get(), TextureMapping.cube(new ResourceLocation(ArtsAndCrafts.MOD_ID, "block/chalk/cyan_chalk")), ModelTemplates.CUBE_ALL);
-        blockStateModelGenerator.createTrivialBlock(KekeBlocks.GRAY_CHALK.get(), TextureMapping.cube(new ResourceLocation(ArtsAndCrafts.MOD_ID, "block/chalk/gray_chalk")), ModelTemplates.CUBE_ALL);
-        blockStateModelGenerator.createTrivialBlock(KekeBlocks.LIGHT_GRAY_CHALK.get(), TextureMapping.cube(new ResourceLocation(ArtsAndCrafts.MOD_ID, "block/chalk/light_gray_chalk")), ModelTemplates.CUBE_ALL);
-        blockStateModelGenerator.createTrivialBlock(KekeBlocks.GREEN_CHALK.get(), TextureMapping.cube(new ResourceLocation(ArtsAndCrafts.MOD_ID, "block/chalk/green_chalk")), ModelTemplates.CUBE_ALL);
-        blockStateModelGenerator.createTrivialBlock(KekeBlocks.LIME_CHALK.get(), TextureMapping.cube(new ResourceLocation(ArtsAndCrafts.MOD_ID, "block/chalk/lime_chalk")), ModelTemplates.CUBE_ALL);
-        blockStateModelGenerator.createTrivialBlock(KekeBlocks.MAGENTA_CHALK.get(), TextureMapping.cube(new ResourceLocation(ArtsAndCrafts.MOD_ID, "block/chalk/magenta_chalk")), ModelTemplates.CUBE_ALL);
-        blockStateModelGenerator.createTrivialBlock(KekeBlocks.ORANGE_CHALK.get(), TextureMapping.cube(new ResourceLocation(ArtsAndCrafts.MOD_ID, "block/chalk/orange_chalk")), ModelTemplates.CUBE_ALL);
-        blockStateModelGenerator.createTrivialBlock(KekeBlocks.PINK_CHALK.get(), TextureMapping.cube(new ResourceLocation(ArtsAndCrafts.MOD_ID, "block/chalk/pink_chalk")), ModelTemplates.CUBE_ALL);
-        blockStateModelGenerator.createTrivialBlock(KekeBlocks.PURPLE_CHALK.get(), TextureMapping.cube(new ResourceLocation(ArtsAndCrafts.MOD_ID, "block/chalk/purple_chalk")), ModelTemplates.CUBE_ALL);
-        blockStateModelGenerator.createTrivialBlock(KekeBlocks.RED_CHALK.get(), TextureMapping.cube(new ResourceLocation(ArtsAndCrafts.MOD_ID, "block/chalk/red_chalk")), ModelTemplates.CUBE_ALL);
-        blockStateModelGenerator.createTrivialBlock(KekeBlocks.YELLOW_CHALK.get(), TextureMapping.cube(new ResourceLocation(ArtsAndCrafts.MOD_ID, "block/chalk/yellow_chalk")), ModelTemplates.CUBE_ALL);
+
+
+        KekeBlockFamilies.getFamilies().filter(BlockFamily::shouldGenerateModel).forEach(family -> blockStateModelGenerator.family(family.getBaseBlock()).generateFor((BlockFamily)family));
 
     }
 
--- fabric/src/main/java/com/kekecreations/arts_and_crafts/fabric/core/datagen/server/ArtsAndCraftsBlockTagGenerator.java
+++ fabric/src/main/java/com/kekecreations/arts_and_crafts/fabric/core/datagen/server/ArtsAndCraftsBlockTagGenerator.java
@@ -18,7 +18,74 @@ public class ArtsAndCraftsBlockTagGenerator extends FabricTagProvider.BlockTagPr
     @Override
     protected void addTags(HolderLookup.Provider arg) {
         appendPickaxes();
+        appendWalls();
+        appendSlabs();
+        appendStairs();
     }
+
+
+    private void appendWalls() {
+        this.getOrCreateTagBuilder(BlockTags.WALLS)
+                .setReplace(false)
+                .add(KekeBlocks.BLACK_CHALK_WALL.get())
+                .add(KekeBlocks.BLUE_CHALK_WALL.get())
+                .add(KekeBlocks.BROWN_CHALK_WALL.get())
+                .add(KekeBlocks.WHITE_CHALK_WALL.get())
+                .add(KekeBlocks.LIGHT_BLUE_CHALK_WALL.get())
+                .add(KekeBlocks.GRAY_CHALK_WALL.get())
+                .add(KekeBlocks.CYAN_CHALK_WALL.get())
+                .add(KekeBlocks.LIGHT_GRAY_CHALK_WALL.get())
+                .add(KekeBlocks.GREEN_CHALK_WALL.get())
+                .add(KekeBlocks.LIME_CHALK_WALL.get())
+                .add(KekeBlocks.MAGENTA_CHALK_WALL.get())
+                .add(KekeBlocks.ORANGE_CHALK_WALL.get())
+                .add(KekeBlocks.PINK_CHALK_WALL.get())
+                .add(KekeBlocks.PURPLE_CHALK_WALL.get())
+                .add(KekeBlocks.RED_CHALK_WALL.get())
+                .add(KekeBlocks.YELLOW_CHALK_WALL.get());
+    }
+    private void appendSlabs() {
+        this.getOrCreateTagBuilder(BlockTags.SLABS)
+                .setReplace(false)
+                .add(KekeBlocks.BLACK_CHALK_SLAB.get())
+                .add(KekeBlocks.BLUE_CHALK_SLAB.get())
+                .add(KekeBlocks.BROWN_CHALK_SLAB.get())
+                .add(KekeBlocks.WHITE_CHALK_SLAB.get())
+                .add(KekeBlocks.LIGHT_BLUE_CHALK_SLAB.get())
+                .add(KekeBlocks.GRAY_CHALK_SLAB.get())
+                .add(KekeBlocks.CYAN_CHALK_SLAB.get())
+                .add(KekeBlocks.LIGHT_GRAY_CHALK_SLAB.get())
+                .add(KekeBlocks.GREEN_CHALK_SLAB.get())
+                .add(KekeBlocks.LIME_CHALK_SLAB.get())
+                .add(KekeBlocks.MAGENTA_CHALK_SLAB.get())
+                .add(KekeBlocks.ORANGE_CHALK_SLAB.get())
+                .add(KekeBlocks.PINK_CHALK_SLAB.get())
+                .add(KekeBlocks.PURPLE_CHALK_SLAB.get())
+                .add(KekeBlocks.RED_CHALK_SLAB.get())
+                .add(KekeBlocks.YELLOW_CHALK_SLAB.get());
+    }
+    private void appendStairs() {
+        this.getOrCreateTagBuilder(BlockTags.STAIRS)
+                .setReplace(false)
+                .add(KekeBlocks.BLACK_CHALK_STAIRS.get())
+                .add(KekeBlocks.BLUE_CHALK_STAIRS.get())
+                .add(KekeBlocks.BROWN_CHALK_STAIRS.get())
+                .add(KekeBlocks.WHITE_CHALK_STAIRS.get())
+                .add(KekeBlocks.LIGHT_BLUE_CHALK_STAIRS.get())
+                .add(KekeBlocks.GRAY_CHALK_STAIRS.get())
+                .add(KekeBlocks.CYAN_CHALK_STAIRS.get())
+                .add(KekeBlocks.LIGHT_GRAY_CHALK_STAIRS.get())
+                .add(KekeBlocks.GREEN_CHALK_STAIRS.get())
+                .add(KekeBlocks.LIME_CHALK_STAIRS.get())
+                .add(KekeBlocks.MAGENTA_CHALK_STAIRS.get())
+                .add(KekeBlocks.ORANGE_CHALK_STAIRS.get())
+                .add(KekeBlocks.PINK_CHALK_STAIRS.get())
+                .add(KekeBlocks.PURPLE_CHALK_STAIRS.get())
+                .add(KekeBlocks.RED_CHALK_STAIRS.get())
+                .add(KekeBlocks.YELLOW_CHALK_STAIRS.get());
+    }
+
+
     private void appendPickaxes() {
         this.getOrCreateTagBuilder(BlockTags.MINEABLE_WITH_PICKAXE)
                 .setReplace(false)
@@ -37,6 +104,54 @@ public class ArtsAndCraftsBlockTagGenerator extends FabricTagProvider.BlockTagPr
                 .add(KekeBlocks.PINK_CHALK.get())
                 .add(KekeBlocks.PURPLE_CHALK.get())
                 .add(KekeBlocks.RED_CHALK.get())
-                .add(KekeBlocks.YELLOW_CHALK.get());
+                .add(KekeBlocks.YELLOW_CHALK.get())
+                .add(KekeBlocks.BLACK_CHALK_WALL.get())
+                .add(KekeBlocks.BLUE_CHALK_WALL.get())
+                .add(KekeBlocks.BROWN_CHALK_WALL.get())
+                .add(KekeBlocks.WHITE_CHALK_WALL.get())
+                .add(KekeBlocks.LIGHT_BLUE_CHALK_WALL.get())
+                .add(KekeBlocks.GRAY_CHALK_WALL.get())
+                .add(KekeBlocks.CYAN_CHALK_WALL.get())
+                .add(KekeBlocks.LIGHT_GRAY_CHALK_WALL.get())
+                .add(KekeBlocks.GREEN_CHALK_WALL.get())
+                .add(KekeBlocks.LIME_CHALK_WALL.get())
+                .add(KekeBlocks.MAGENTA_CHALK_WALL.get())
+                .add(KekeBlocks.ORANGE_CHALK_WALL.get())
+                .add(KekeBlocks.PINK_CHALK_WALL.get())
+                .add(KekeBlocks.PURPLE_CHALK_WALL.get())
+                .add(KekeBlocks.RED_CHALK_WALL.get())
+                .add(KekeBlocks.YELLOW_CHALK_WALL.get())
+                .add(KekeBlocks.BLACK_CHALK_SLAB.get())
+                .add(KekeBlocks.BLUE_CHALK_SLAB.get())
+                .add(KekeBlocks.BROWN_CHALK_SLAB.get())
+                .add(KekeBlocks.WHITE_CHALK_SLAB.get())
+                .add(KekeBlocks.LIGHT_BLUE_CHALK_SLAB.get())
+                .add(KekeBlocks.GRAY_CHALK_SLAB.get())
+                .add(KekeBlocks.CYAN_CHALK_SLAB.get())
+                .add(KekeBlocks.LIGHT_GRAY_CHALK_SLAB.get())
+                .add(KekeBlocks.GREEN_CHALK_SLAB.get())
+                .add(KekeBlocks.LIME_CHALK_SLAB.get())
+                .add(KekeBlocks.MAGENTA_CHALK_SLAB.get())
+                .add(KekeBlocks.ORANGE_CHALK_SLAB.get())
+                .add(KekeBlocks.PINK_CHALK_SLAB.get())
+                .add(KekeBlocks.PURPLE_CHALK_SLAB.get())
+                .add(KekeBlocks.RED_CHALK_SLAB.get())
+                .add(KekeBlocks.YELLOW_CHALK_SLAB.get())
+                .add(KekeBlocks.BLACK_CHALK_STAIRS.get())
+                .add(KekeBlocks.BLUE_CHALK_STAIRS.get())
+                .add(KekeBlocks.BROWN_CHALK_STAIRS.get())
+                .add(KekeBlocks.WHITE_CHALK_STAIRS.get())
+                .add(KekeBlocks.LIGHT_BLUE_CHALK_STAIRS.get())
+                .add(KekeBlocks.GRAY_CHALK_STAIRS.get())
+                .add(KekeBlocks.CYAN_CHALK_STAIRS.get())
+                .add(KekeBlocks.LIGHT_GRAY_CHALK_STAIRS.get())
+                .add(KekeBlocks.GREEN_CHALK_STAIRS.get())
+                .add(KekeBlocks.LIME_CHALK_STAIRS.get())
+                .add(KekeBlocks.MAGENTA_CHALK_STAIRS.get())
+                .add(KekeBlocks.ORANGE_CHALK_STAIRS.get())
+                .add(KekeBlocks.PINK_CHALK_STAIRS.get())
+                .add(KekeBlocks.PURPLE_CHALK_STAIRS.get())
+                .add(KekeBlocks.RED_CHALK_STAIRS.get())
+                .add(KekeBlocks.YELLOW_CHALK_STAIRS.get());
     }
 }
