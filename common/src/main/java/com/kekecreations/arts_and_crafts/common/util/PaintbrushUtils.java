3ddbb0b6c1f3334fe9252df3e30006d1f07b3271
KekeCreations <89083260+KekeCreations@users.noreply.github.com>
2024-05-28 02:25:54 +0100

    Paintbrush gives Brush when broken

--- common/src/main/java/com/kekecreations/arts_and_crafts/common/util/PaintbrushUtils.java
+++ common/src/main/java/com/kekecreations/arts_and_crafts/common/util/PaintbrushUtils.java
@@ -13,6 +13,7 @@ import net.minecraft.world.InteractionHand;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.DyeColor;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.entity.BlockEntity;
@@ -44,6 +45,12 @@ public class PaintbrushUtils {
             }
             blockState.getBlock().setPlacedBy(level, pos, blockState, player, itemStack);
             itemStack.hurtAndBreak(1, player, (entity) -> entity.broadcastBreakEvent(hand));
+            if (itemStack.isEmpty()) {
+                ItemStack itemStack2 = new ItemStack(Items.BRUSH);
+                itemStack2.setTag(itemStack.getTag());
+                player.setItemInHand(hand, itemStack2);
+            }
+
         }
 
     }
