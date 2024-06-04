e456d813e4bf42bf02acc1a130a9896c6a51086f
KekeCreations <89083260+KekeCreations@users.noreply.github.com>
2024-06-05 00:16:37 +0100

    Paintbrush Datagen Part 3

--- common/src/main/java/com/kekecreations/arts_and_crafts/core/mixin/ShulkerBoxBlockMixin.java
+++ common/src/main/java/com/kekecreations/arts_and_crafts/core/mixin/ShulkerBoxBlockMixin.java
@@ -1,15 +1,14 @@
 package com.kekecreations.arts_and_crafts.core.mixin;
 
 import com.kekecreations.arts_and_crafts.common.item.PaintbrushItem;
-import com.kekecreations.arts_and_crafts.common.util.ArtsAndCraftsDyedBlockLists;
 import com.kekecreations.arts_and_crafts.common.util.PaintbrushUtils;
-import com.kekecreations.arts_and_crafts.core.config.ArtsAndCraftsCommonConfig;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.ShulkerBoxBlock;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.ShulkerBoxBlockEntity;
@@ -28,19 +27,18 @@ public class ShulkerBoxBlockMixin {
 
     @Inject(method = "use", at = @At(value= "INVOKE", target = "net/minecraft/world/level/Level.getBlockEntity (Lnet/minecraft/core/BlockPos;)Lnet/minecraft/world/level/block/entity/BlockEntity;"), cancellable = true)
     public void arts_and_crafts_use(BlockState blockState, Level level, BlockPos blockPos, Player player, InteractionHand interactionHand, BlockHitResult blockHitResult, CallbackInfoReturnable<InteractionResult> cir) {
-        if (ArtsAndCraftsCommonConfig.CAN_PAINT_SHULKER_BOX.get()) {
-            List<ItemStack> itemList = new ArrayList<ItemStack>();
-            BlockEntity blockEntity = level.getBlockEntity(blockPos);
-            if (blockEntity instanceof ShulkerBoxBlockEntity shulkerBE) {
-                for (int i = 0; i < shulkerBE.getContainerSize(); ++i) {
-                    itemList.add(shulkerBE.getItem(i));
+        ItemStack itemStack = player.getItemInHand(interactionHand);
+        if (itemStack.getItem() instanceof PaintbrushItem) {
+            Block finalBlock = PaintbrushUtils.getFinalBlock(level.registryAccess(), blockState, itemStack);
+            if (finalBlock != null && finalBlock != blockState.getBlock()) {
+                List<ItemStack> itemList = new ArrayList<ItemStack>();
+                BlockEntity blockEntity = level.getBlockEntity(blockPos);
+                if (blockEntity instanceof ShulkerBoxBlockEntity shulkerBE) {
+                    for (int i = 0; i < shulkerBE.getContainerSize(); ++i) {
+                        itemList.add(shulkerBE.getItem(i));
+                    }
                 }
-
-            }
-            ItemStack itemStack = player.getItemInHand(interactionHand);
-            ShulkerBoxBlock $this = (ShulkerBoxBlock)(Object)this;
-            if (itemStack.getItem() instanceof PaintbrushItem paintBrushItem && $this.getColor() != paintBrushItem.getDyeColor()) {
-                PaintbrushUtils.paintShulkerBox(level, ArtsAndCraftsDyedBlockLists.getDyedShulkerBox(paintBrushItem.getDyeColor().getId()).defaultBlockState(), blockPos, player, itemStack, interactionHand);
+                PaintbrushUtils.paintBlock(level, finalBlock.defaultBlockState(), blockPos, player, itemStack, interactionHand);
                 BlockEntity newBlockEntity = level.getBlockEntity(blockPos);
                 if (newBlockEntity instanceof ShulkerBoxBlockEntity newShulkerBE) {
                     for (int i = 0; i < newShulkerBE.getContainerSize(); ++i) {
