80312b10fa133b9643161209e7ecf07002515433
KekeCreations <89083260+KekeCreations@users.noreply.github.com>
2024-06-05 01:09:03 +0100

    Paintbrush Datapack done

--- common/src/main/java/com/kekecreations/arts_and_crafts/core/mixin/BedBlockMixin.java
+++ common/src/main/java/com/kekecreations/arts_and_crafts/core/mixin/BedBlockMixin.java
@@ -1,9 +1,7 @@
 package com.kekecreations.arts_and_crafts.core.mixin;
 
 import com.kekecreations.arts_and_crafts.common.item.PaintbrushItem;
-import com.kekecreations.arts_and_crafts.common.util.ArtsAndCraftsDyedBlockLists;
 import com.kekecreations.arts_and_crafts.common.util.PaintbrushUtils;
-import com.kekecreations.arts_and_crafts.core.config.ArtsAndCraftsCommonConfig;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
@@ -11,6 +9,7 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.BedBlock;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.BlockHitResult;
 import org.spongepowered.asm.mixin.Mixin;
@@ -23,15 +22,12 @@ public class BedBlockMixin {
 
     @Inject(method = "use", at = @At(value= "HEAD"), cancellable = true)
     public void arts_and_crafts_use(BlockState blockState, Level level, BlockPos blockPos, Player player, InteractionHand interactionHand, BlockHitResult blockHitResult, CallbackInfoReturnable<InteractionResult> cir) {
-        if (ArtsAndCraftsCommonConfig.CAN_PAINT_BED.get()) {
-            ItemStack itemStack = player.getItemInHand(interactionHand);
-            if (!level.isClientSide()) {
-                if (itemStack.getItem() instanceof PaintbrushItem paintBrushItem) {
-                    if (level.getBlockState(blockPos).getBlock() instanceof BedBlock bedBlock && bedBlock.getColor() != paintBrushItem.getDyeColor()) {
-                        PaintbrushUtils.paintBed(level, ArtsAndCraftsDyedBlockLists.getDyedBed(paintBrushItem.getDyeColor().getId()).defaultBlockState(), blockPos, player, itemStack, interactionHand);
-                        cir.setReturnValue(InteractionResult.SUCCESS);
-                    }
-                }
+        ItemStack itemStack = player.getItemInHand(interactionHand);
+        if (itemStack.getItem() instanceof PaintbrushItem) {
+            Block finalBlock = PaintbrushUtils.getFinalBlock(level.registryAccess(), blockState, itemStack);
+            if (finalBlock != null && finalBlock != blockState.getBlock()) {
+                PaintbrushUtils.paintBed(level, finalBlock.defaultBlockState(), blockPos, player, itemStack, interactionHand);
+                cir.setReturnValue(InteractionResult.SUCCESS);
             }
         }
     }
