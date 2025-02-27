c7381976bb25213acbe675ef75a4b014b704cc54
KekeCreations <89083260+KekeCreations@users.noreply.github.com>
2025-02-27 00:18:32 +0000

    Removed Redundant Classes + Mixins

--- common/src/main/java/com/kekecreations/arts_and_crafts/common/block/ACFlammableFenceBlock.java
+++
@@ -1,9 +0,0 @@
-package com.kekecreations.arts_and_crafts.common.block;
-
-import net.minecraft.world.level.block.FenceBlock;
-
-public class ACFlammableFenceBlock extends FenceBlock {
-    public ACFlammableFenceBlock(Properties $$0) {
-        super($$0);
-    }
-}
--- common/src/main/java/com/kekecreations/arts_and_crafts/common/block/ACFlammableLeavesBlock.java
+++
@@ -1,9 +0,0 @@
-package com.kekecreations.arts_and_crafts.common.block;
-
-import net.minecraft.world.level.block.LeavesBlock;
-
-public class ACFlammableLeavesBlock extends LeavesBlock {
-    public ACFlammableLeavesBlock(Properties $$0) {
-        super($$0);
-    }
-}
