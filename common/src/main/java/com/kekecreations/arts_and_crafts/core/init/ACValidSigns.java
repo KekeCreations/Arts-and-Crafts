d899731fec6e032980eb7530ff9677b80ee92c56
KekeCreations <89083260+KekeCreations@users.noreply.github.com>
2025-03-04 21:34:21 +0000

    Updated JinxedLib and Dropped 1.21

--- common/src/main/java/com/kekecreations/arts_and_crafts/core/init/ACValidSigns.java
+++ common/src/main/java/com/kekecreations/arts_and_crafts/core/init/ACValidSigns.java
@@ -1,14 +1,14 @@
 package com.kekecreations.arts_and_crafts.core.init;
 
 import com.kekecreations.arts_and_crafts.core.registry.ACBlocks;
-import com.kekecreations.jinxedlib.core.util.JinxedSignUtils;
+import com.kekecreations.jinxedlib.core.util.JinxedSignHelper;
 
 public class ACValidSigns {
 
     public static void add() {
-        if (!JinxedSignUtils.SIGN_IS_VALID.contains(ACBlocks.CORK_SIGN.get())) { JinxedSignUtils.makeSignValid(ACBlocks.CORK_SIGN.get()); }
-        if (!JinxedSignUtils.SIGN_IS_VALID.contains(ACBlocks.CORK_WALL_SIGN.get())) { JinxedSignUtils.makeSignValid(ACBlocks.CORK_WALL_SIGN.get()); }
-        if (!JinxedSignUtils.HANGING_SIGN_IS_VALID.contains(ACBlocks.CORK_HANGING_SIGN.get())) { JinxedSignUtils.makeHangingSignValid(ACBlocks.CORK_HANGING_SIGN.get()); }
-        if (!JinxedSignUtils.HANGING_SIGN_IS_VALID.contains(ACBlocks.CORK_WALL_HANGING_SIGN.get())) { JinxedSignUtils.makeHangingSignValid(ACBlocks.CORK_WALL_HANGING_SIGN.get()); }
+        JinxedSignHelper.makeSignValid(ACBlocks.CORK_SIGN.get());
+        JinxedSignHelper.makeSignValid(ACBlocks.CORK_WALL_SIGN.get());
+        JinxedSignHelper.makeHangingSignValid(ACBlocks.CORK_HANGING_SIGN.get());
+        JinxedSignHelper.makeHangingSignValid(ACBlocks.CORK_WALL_HANGING_SIGN.get());
     }
 }
