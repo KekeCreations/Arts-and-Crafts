780b1de72c8dede2760e075dc997383889c907f5
KekeCreations <89083260+KekeCreations@users.noreply.github.com>
2025-03-05 01:22:17 +0000

    Compostables now in JSON + Fixed Cork Log

--- fabric/src/main/java/com/kekecreations/arts_and_crafts/fabric/core/registry/ACFabricRegistries.java
+++ fabric/src/main/java/com/kekecreations/arts_and_crafts/fabric/core/registry/ACFabricRegistries.java
@@ -1,7 +1,6 @@
 package com.kekecreations.arts_and_crafts.fabric.core.registry;
 
 import com.kekecreations.arts_and_crafts.core.registry.ACBlocks;
-import net.fabricmc.fabric.api.registry.CompostingChanceRegistry;
 import net.fabricmc.fabric.api.registry.FlammableBlockRegistry;
 import net.fabricmc.fabric.api.registry.StrippableBlockRegistry;
 
@@ -25,9 +24,5 @@ public class ACFabricRegistries {
         //STRIPPABLE
         StrippableBlockRegistry.register(ACBlocks.CORK_LOG.get(), ACBlocks.STRIPPED_CORK_LOG.get());
         StrippableBlockRegistry.register(ACBlocks.CORK_WOOD.get(), ACBlocks.STRIPPED_CORK_WOOD.get());
-
-        //COMPOSTING
-        CompostingChanceRegistry.INSTANCE.add(ACBlocks.CORK_SAPLING.get(), 0.3F);
-        CompostingChanceRegistry.INSTANCE.add(ACBlocks.CORK_LEAVES.get(), 0.3F);
     }
 }
