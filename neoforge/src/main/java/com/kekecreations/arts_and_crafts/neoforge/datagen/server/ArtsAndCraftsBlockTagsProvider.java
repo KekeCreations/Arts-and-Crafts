1292054ccf05d572cd209cd0cc6ebaad12863a1b
KekeCreations <89083260+KekeCreations@users.noreply.github.com>
2025-07-08 23:54:25 +0100

    Ported Minor Features

--- neoforge/src/main/java/com/kekecreations/arts_and_crafts/neoforge/datagen/server/ArtsAndCraftsBlockTagsProvider.java
+++ neoforge/src/main/java/com/kekecreations/arts_and_crafts/neoforge/datagen/server/ArtsAndCraftsBlockTagsProvider.java
@@ -43,7 +43,53 @@ public class ArtsAndCraftsBlockTagsProvider extends BlockTagsProvider {
         appendChalkDust();
         appendWoolCarpets();
         appendWool();
+        appendShovels();
+        appendBeds();
+        appendAllSigns();
+        appendWallSigns();
+        appendSigns();
+        appendAllHangingSigns();
+        appendHangingSigns();
+        appendWallHangingSigns();
     }
+
+    private void appendBeds() {
+        this.tag(BlockTags.BEDS)
+                .add(ACBlocks.BLEACHED_BED.get());
+    }
+
+    private void appendAllSigns() {
+        this.tag(BlockTags.ALL_SIGNS)
+                .add(ACBlocks.CORK_HANGING_SIGN.get())
+                .add(ACBlocks.CORK_WALL_HANGING_SIGN.get());
+    }
+
+    private void appendWallSigns() {
+        this.tag(BlockTags.WALL_SIGNS)
+                .add(ACBlocks.CORK_WALL_HANGING_SIGN.get());
+    }
+
+    private void appendSigns() {
+        this.tag(BlockTags.SIGNS)
+                .add(ACBlocks.CORK_HANGING_SIGN.get());
+    }
+
+    private void appendAllHangingSigns() {
+        this.tag(BlockTags.ALL_HANGING_SIGNS)
+                .add(ACBlocks.CORK_HANGING_SIGN.get())
+                .add(ACBlocks.CORK_WALL_HANGING_SIGN.get());
+    }
+
+    private void appendHangingSigns() {
+        this.tag(BlockTags.CEILING_HANGING_SIGNS)
+                .add(ACBlocks.CORK_HANGING_SIGN.get());
+    }
+
+    private void appendWallHangingSigns() {
+        this.tag(BlockTags.CEILING_HANGING_SIGNS)
+                .add(ACBlocks.CORK_WALL_HANGING_SIGN.get());
+    }
+
 
     private void appendWool() {
         this.tag(BlockTags.WOOL)
@@ -188,8 +234,15 @@ public class ArtsAndCraftsBlockTagsProvider extends BlockTagsProvider {
     }
 
 
+    private void appendShovels() {
+        this.tag(BlockTags.MINEABLE_WITH_SHOVEL)
+                .add(ACBlocks.BLEACHED_CONCRETE_POWDER.get());
+    }
+
     private void appendPickaxes() {
         this.tag(BlockTags.MINEABLE_WITH_PICKAXE)
+                .add(ACBlocks.BLEACHED_CHALK.get())
+                .add(ACBlocks.BLEACHED_CONCRETE.get())
                 .add(ACBlocks.GLAZED_TERRACOTTA.get())
                 .add(ACBlocks.TERRACOTTA_SHINGLES.get())
                 .add(ACBlocks.TERRACOTTA_SHINGLE_SLAB.get())
